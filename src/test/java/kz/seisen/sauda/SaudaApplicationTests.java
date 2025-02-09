f90933784046d311efb6eb22b578dbe800427d62
aloneen <seisenbek2005@mail.ru>
2025-02-09 17:39:44 +0500

    First commit

---
+++ src/test/java/kz/seisen/sauda/SaudaApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.seisen.sauda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaudaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
